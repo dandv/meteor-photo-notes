239f82a296d427a62a4e3f1440ded6b142712042
Dan Dascalescu <ddascalescu+github@gmail.com>
2015-03-26 21:18:56 -0700

    Add text notes

--- client/photo.js
+++ client/photo.js
@@ -1,3 +1,9 @@
+var cameraOptions = {
+  width: 800,
+  height: 600
+};
+
+// provide the value for `notes` in {{#each notes}} in the HTML
 Template.body.helpers({
   notes: function () {
     return Notes.find(
@@ -9,21 +15,32 @@ Template.body.helpers({
   }  
 });
 
+// Define the event map - http://docs.meteor.com/#/full/eventmaps
 Template.body.events({
-  'click button': function () {
-    var cameraOptions = {
-      width: 800,
-      height: 600
-    };
-
+  'click #take-photo': function () {
     MeteorCamera.getPicture(cameraOptions, function (error, data) {
       Session.set("photo", data);
       if (error) {
         // e.g. camera permission denied, or unsupported browser (Safari on iOS, looking at you)
         console.log(error);
       } else {  
-        Notes.insert({photo: data, timestamp: new Date()});
+        Notes.insert({
+          photo: data,
+          timestamp: new Date()
+        });
       }  
     });
+  },
+  
+  'submit form': function (event, template) {
+    var noteText = template.find('input').value;
+    if (noteText) {
+      Notes.insert({
+        note: noteText,
+        timestamp: new Date()
+      });
+      template.find('input').value = '';  // clear the field after saving
+    }
+    return false;  // calls preventDefault() to not submit the form
   }
 });
